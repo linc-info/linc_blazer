c0a3bfa84f1190179940e9c4c9acfdce705cef15
wicros824 <wicroswts@gmail.com>
2019-01-24 19:43:22 +0900

    Set up deploy staging

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 server 'edusoho_blazer', user: "deploy", roles: %w{app db web}
 set :rails_env, "production"
-ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `master`.chomp
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,3 @@
 server 'staging_blazer', user: "deploy", roles: %w{app db web}
 set :rails_env, "production"
-ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, "staging"
