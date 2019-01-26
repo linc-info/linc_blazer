4c1ae2767bb41fefd34997aa6beacbdadf9a8afd
PeterChen <mitpig@gmail.com>
2019-01-26 13:29:55 +0900

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      get 'api-docs', to: 'docs#index'
       post 'users', to: 'sign#sign_up'
       post 'users/sign_in', to: 'sign#sign_in'
       post 'users/sign_out', to: 'sign#sign_out'
