c09f99926654730c5e5f6a3219a2bd90c3f2e2dd
wicros824 <wicroswts@gmail.com>
2019-02-22 19:22:23 +0900

    Fix LP size

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  skip_before_action :verify_authenticity_token
+  protect_from_forgery prepend: true, with: :exception
 
   protected
 
